9320e79bbd7801b9e69078440000cf75d450dcd9
unknown <poudelaayush@gmail.com>
2025-08-18 12:39:31 +0545

    Framework

---
+++ gov/src/ManageBranch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManageBranch = () => {
+  return (
+    <div>ManageBranch</div>
+  )
+}
+
+export default ManageBranch
--- gov/src/MyRoutes.jsx
+++ gov/src/MyRoutes.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import { Home } from './Home';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './Home';
+import AuditLogs from './AuditLogs';
+import ManageBranch from './ManageBranch';
+import MyProfile from './MyProfile';
+import SystemOverview from './SystemOverview';
+import SystemSetting from './SystemSetting';
+import FinancialReports from './FinancialReports';
+import UserManagement from './UserManagement';
+
+// Import the root CSS file
+import './index.css';
+
 function MyRoutes() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-        </Routes>
-        </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/audit-logs" element={<AuditLogs />} />
+        <Route path="/manage-branch" element={<ManageBranch />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/system-overview" element={<SystemOverview />} />
+        <Route path="/system-setting" element={<SystemSetting />} />
+        <Route path="/financial-report" element={<FinancialReports />} />
+        <Route path="/user-management" element={<UserManagement />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-export default MyRoutes
+
+export default MyRoutes;
